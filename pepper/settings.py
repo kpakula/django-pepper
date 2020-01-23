91f0cfedd1da4f876ffd142ed5a55007273caa7c
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-23 12:47:17 +0100

    Connected with postgreSql database

--- pepper/settings.py
+++ pepper/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pepper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pepperDb',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+1'
 
 USE_I18N = True
 
