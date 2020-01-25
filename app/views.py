6111d2f74f18f696269ee739e9ab69bdff9d8a13
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-01-25 09:04:46 +0100

    Updated login and register view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import  authenticate
+from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
 # Create your views here.
 
@@ -16,7 +17,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
             return redirect('/')
     else:
         form = UserCreationForm()
@@ -25,4 +26,20 @@ def register(request):
 
 
 def login(request):
-    return HttpResponse("Sign in")
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('/app/')
+            else:
+                print('invalid')
+                # messages.error(request, "Invalid username or password.")
+        else:
+            print('invalid')
+            # messages.error(request, "Invalid username or password. ")
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
